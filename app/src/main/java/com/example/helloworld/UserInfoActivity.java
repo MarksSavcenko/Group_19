a1395ed8ed1d83837f2427ff3a49d3756a9d807b
Conor Power <23369779@studentmail.ul.ie>
2026-03-23 17:34:32 +0000

    create user info file

---
+++ app/src/main/java/com/example/helloworld/UserInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedinstanceState){
+        super.onCreate(savedinstanceState);
+        setContentView(R.layout.user_info);
+    }
+
+}
